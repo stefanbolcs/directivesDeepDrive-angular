5c8fef964680d877039d988a4503de23597ffbe2
Stefan B <stefanbolcs@gmail.com>
2020-11-16 22:00:36 +0100

    using @HostListener  to style element(mouseenter, mouseleave)

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,24 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  
 
+  //for any DOM manipulations you should use the Renderer!!! look at Renderer methods for more info
   constructor(private elRef:ElementRef,private renderer: Renderer2 ){}
 
   ngOnInit(){
+    // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event){
     this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');  jasmine-core 3.5   typescript 3.6
   }
 
 }
